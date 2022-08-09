8efd4de40f7943058920d8e406e13c7a53ce6d18
mugdha273 <mugdhasharma0327@gmail.com>
2022-08-09 15:53:01 +0530

    setttings update

--- api/settings.py
+++ api/settings.py
@@ -16,7 +16,7 @@ import django_heroku
 from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -30,7 +30,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 # Application definition
 
 INSTALLED_APPS = [
