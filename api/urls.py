70d991375ca41814899b109555343c6cfd91cd23
mugdha273 <mugdhasharma0327@gmail.com>
2022-08-22 21:34:45 +0530

    fixes typo

--- api/urls.py
+++ api/urls.py
@@ -17,13 +17,14 @@ from django.contrib import admin
 from django.urls import path,include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('',include('base.urls')),
     path('adminpanel/',include('adminpanel.urls')),
-    path('userpanel/',include('userpanel.urls'))
-    re_path(r^'media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT'})
+    path('userpanel/',include('userpanel.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
