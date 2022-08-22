7ba2a18eb37745612aa7ee4756558052c7506ecd
mugdha273 <mugdhasharma0327@gmail.com>
2022-08-22 21:31:26 +0530

    urls update

--- api/urls.py
+++ api/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -23,6 +23,7 @@ urlpatterns = [
     path('',include('base.urls')),
     path('adminpanel/',include('adminpanel.urls')),
     path('userpanel/',include('userpanel.urls'))
+    re_path(r^'media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT'})
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
